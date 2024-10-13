a1412d298fa0fd946f2d07ef892fd44780c8c9dc
Silas Bartha <silas@exvacuum.dev>
2024-10-13 14:21:36 -0400

    Sun Oct 13 02:21:36 PM EDT 2024

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,13 @@ vim.o.clipboard = "unnamedplus"
 vim.o.conceallevel = 3
 vim.o.termguicolors = true
 
+-- Settings for enforcing good habits
+vim.o.mouse=''
+vim.keymap.set({'n', 'i', 'v'}, '<Up>', '<Nop>');
+vim.keymap.set({'n', 'i', 'v'}, '<Down>', '<Nop>');
+vim.keymap.set({'n', 'i', 'v'}, '<Left>', '<Nop>');
+vim.keymap.set({'n', 'i', 'v'}, '<Right>', '<Nop>');
+
 require("config.lazy")
 
 require("config.yarnspinner")
